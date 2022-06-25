54e256198aeeb844cff6f1c2d35c741eda3aec89
cedricorais <107686710+cedricorais@users.noreply.github.com>
2022-06-25 18:01:25 +0800

    Tweaked some files

---
+++ CemeteryManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using mvcnoauth.DataTest;
+using mvcnoauth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcnoauth.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<TestModel> test = new List<TestModel>();
+            test.Add(new TestModel("19-00000", "asd", "asd", "asd"));
+            TestDAO testdao = new TestDAO();
+            test = testdao.getData();
+
+            return View("Index", test);
+        }
+    }
+}
